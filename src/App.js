41f8591e3df90822dbfcbb66c5fca26aa814472c
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2025-01-19 01:49:02 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -104,6 +104,10 @@ function App() {
           <Button variant="outline-warning" onClick={stop}>Stop</Button>
           <Button variant="outline-dark" onClick={reset}>Reset</Button>
         </div>
+
+  <div style={{text-align: center, margin-top: 30px, color: #0271ff}}>
+        <p>Made By <strong>Azaan Ullah Khan</strong> with 💙</p>
+    </div>
       </div>
     </>
   );
